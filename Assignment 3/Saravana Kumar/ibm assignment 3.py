0c7f834c6b73e025b4ebedf8fc5b9d4de2086070
19BEC4180 <114489737+19BEC4180@users.noreply.github.com>
2022-10-07 09:13:50 +0530

    Update ibm assignment 3.py

--- Assignment 3/Saravana Kumar/ibm assignment 3.py
+++ Assignment 3/Saravana Kumar/ibm assignment 3.py
@@ -1,20 +1,10 @@
-import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-
-while True:
-GPIO.output(11,True) ## Turn on redLed
-time.sleep(1) ## Wait for one second
-GPIO.output(11,False) ## Turn off redLed
-time.sleep(1) ## Wait for one second
-GPIO.output(12,True) ## Turn on yellowLed
-time.sleep(1) ## Wait for one second
-GPIO.output(12,False) ## Turn off yellowLed
-time.sleep(1) ## Wait for one second
-GPIO.output(13,True) ## Turn on greenLed
-time.sleep(1) ## Wait for one second
-GPIO.output(13,False) ## Turn off greenLed
-time.sleep(1) ## Wait for one second
+k=int(input("enter the input: "))
+range1=range(1,21,1)
+range2=range(21,41,1)
+range3=range(41,61,1)
+if k in range1:
+  print("red light")
+elif k in range2 :
+  print("yellow light")
+elif k in range3:
+  print("green light")
